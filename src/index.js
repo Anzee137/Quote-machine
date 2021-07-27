7e8a19182894d615701083e9b25c8c223ca2b72a
Atryom-Bohaturou <81459880+Atryom-Bohaturou@users.noreply.github.com>
2021-07-27 15:38:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { Provider } from "react-redux";
 ReactDOM.render(
   <Provider store={store}>
     <App />
+    <a href="https://github.com/Atryom-Bohaturou/Quote-machine">Repository of this project</a>
   </Provider>,
   document.getElementById('root')
 );
